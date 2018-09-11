24351ae831b1b4164bf3a1d031fe4a84b89f64c1
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2018-09-10 19:18:13 -0700

    products.js: use ES6 Set() rather than d3.set

--- public/libs/earth/1.0.0/products.js
+++ public/libs/earth/1.0.0/products.js
@@ -689,7 +689,7 @@ var products = function() {
     }
 
     return {
-        overlayTypes: d3.set(_.keys(FACTORIES)),
+        overlayTypes: new Set(_.keys(FACTORIES)),
         productsFor: productsFor
     };
 
